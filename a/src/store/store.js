08805d464675fe07ff9464472dcfa596f18d4962
jjeris <johans.justs.eris@gmail.com>
2023-11-02 06:37:23 +0200

    Fix download

--- a/src/store/store.js
+++ a/src/store/store.js
@@ -6,12 +6,17 @@ import { download } from "tauri-plugin-upload-api";
 
 export const useTestStore = reactive({
     async testMsg() {
-        // console.log("Cunt");
-        download(
-            "https://builder.blender.org/download/daily/blender-4.1.0-alpha+main.515d25fbedff-windows.amd64-release.zip",
-            "C:\\Users\\J\\Desktop\\BlenderBaseApps\\test.zip",
-            (progress, total) => console.log(`Downloaded ${progress} of ${total} bytes`), // a callback that will be called with the download progress
-            // { "Content-Type": "text/plain" }, // optional headers to send with the request
-          );
+        console.log("https://builder.blender.org/download/daily/archive/blender-4.1.0-alpha+main.f03a01504939-windows.amd64-release.zip");
+        try {
+            download(
+                "https://builder.blender.org/download/daily/archive/blender-4.1.0-alpha+main.f03a01504939-windows.amd64-release.zip",
+                "C:\\Users\\J\\Desktop\\BlenderBaseDirPath\\test8.zip",
+                (progress, total) => console.log(`Downloaded ${progress} of ${total} bytes`), // a callback that will be called with the download progress
+                // { "Content-Type": "text/plain" }, // optional headers to send with the request
+              );
+        } catch (error) {
+            console.log(error);
+        }
+        
     }
 })
